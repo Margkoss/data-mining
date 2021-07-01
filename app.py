e68a2a5e898152e135f459184b8ff0fb0b8f9829
Margkoss <markosg.stamatakis@gmail.com>
2021-07-01 19:51:10 +0300

    Make app non importable

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 from src.main import bootstrap_application
 
-bootstrap_application()
+if __name__ == "__main__":
+    bootstrap_application()
+else:
+    raise Exception("Cannot be imported, meant as standalone")
